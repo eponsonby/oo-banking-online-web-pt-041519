0049a555f7e09e621b0935ce74cf7ab7bb204425
Erin Ponsonby <erinponsonby@gmail.com>
2019-05-19 20:44:49 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -40,7 +40,8 @@ class Transfer
         if @status = "complete"
           @sender.balance = @sender.balance + @amount
           @receiver.balance = @receiver.balance - @amount
-          @status = "reversed"
+        end
+        @status = "reversed"
         end
     end
 
