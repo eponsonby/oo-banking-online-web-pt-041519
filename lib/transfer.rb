9ad44ec3491a931293cfd406dcf93526ef7eec1d
Erin Ponsonby <erinponsonby@gmail.com>
2019-05-19 20:48:29 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -37,7 +37,7 @@ class Transfer
     end
     
     def reverse_transfer
-        if @status = "complete"
+        self.execute_transaction
           @sender.balance = @sender.balance + @amount
           @receiver.balance = @receiver.balance - @amount
           @status = "reversed"
